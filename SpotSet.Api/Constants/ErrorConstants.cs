5bc0ec53af41dc93f99a37a041b0e9288ed6276c
Heather You <heather.fswd@gmail.com>
2019-07-18 01:05:08 -0400

    Change error message

--- SpotSet.Api/Constants/ErrorConstants.cs
+++ SpotSet.Api/Constants/ErrorConstants.cs
@@ -4,7 +4,7 @@ namespace SpotSet.Api.Constants
     {
         public const string SetlistError = "No results found for setlist with an ID of ";
         public const string SetlistErrorTryAgain = ". Please try your search again.";
-        public const string SpotifyError = "There was an error fetching track details for the requested setlist!";
+        public const string SpotifyError = "There was an error fetching track details for the requested setlist. Please make sure it is not an empty setlist.";
         public const string SpotifyAuthError = "There was an error authenticating the app.";
     }
 }
